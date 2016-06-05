d6a2d96304d2dfd55ecb1d9cb0397d9df736c5ee
gastal_r <remi.gastaldi@epitech.eu>
2016-06-05 20:49:55 +0200

    Pour renaud

--- sources/pipe_inf.c
+++ sources/pipe_inf.c
@@ -5,7 +5,7 @@
 ** Login   <gastal_r@epitech.net>
 ** 
 ** Started on  Sat Jun  4 22:09:08 2016 
-** Last update Sun Jun  5 20:27:58 2016 
+** Last update Sun Jun  5 20:49:45 2016 
 */
 
 #include		"42sh.h"
@@ -32,11 +32,9 @@ int			loop_pipe(t_plist *plist, int *in_out, char **cmd, char **env)
   return (pid);
 }
 
-int			pipe_inf(t_plist *plist, t_cmd *cmd, int n, char **env)
+int			pipe_inf(t_plist *plist, t_cmd **cmd, int n, char **env)
 {
   int			i;
-  pid_t			pid;
-  int			in;
   int			fd[2];
   int			in_out[2];
 
@@ -46,13 +44,13 @@ int			pipe_inf(t_plist *plist, t_cmd *cmd, int n, char **env)
     {
       pipe(fd);
       in_out[1] = fd[1];
-      loop_pipe(plist, in_out, cmd->cmd, env);
-      cmd = cmd->next;
+      loop_pipe(plist, in_out, (*cmd)->cmd, env);
+      *cmd = (*cmd)->next;
       close (fd[1]);
       in_out[0] = fd[0];
     }
   if (in_out[0] != 0)
     dup2(in_out[0], 0);
   plist->pipe = 1;
-  return (check_action(cmd->cmd, env, plist));
+  return (check_action((*cmd)->cmd, env, plist));
 }
