c8c404ea254d0fb3af412e727855abc56741bf53
Yassir <yassir.jabbari@epitech.eu>
2016-06-06 14:07:20 +0200

    le gros sig

--- sources/sig.c
+++ sources/sig.c
@@ -5,7 +5,7 @@
 ** Login   <jabbar_y@epitech.net>
 **
 ** Started on  Mon Jun  6 13:31:59 2016 Jabbari Yassir
-** Last update Mon Jun  6 13:40:03 2016 Jabbari Yassir
+** Last update Mon Jun  6 14:00:24 2016 Jabbari Yassir
 */
 
 #include "42sh.h"
@@ -21,5 +21,25 @@ int		sig_func_dad(int pid, int status)
 void		print_sig(int status)
 {
   if (WTERMSIG(status) == SIGSEGV)
-    dprintf(2, "segmentation fault\n");
+    dprintf(2, "Segmentation fault (core dumped)\n");
+  else if (WTERMSIG(status) == SIGHUP)
+    dprintf(2, "Hangup\n");
+  else if (WTERMSIG(status) == SIGQUIT)
+    dprintf(2, "Quit (core dumped)\n");
+  else if (WTERMSIG(status) == SIGILL)
+    dprintf(2, "Illegal instruction (core dumped)\n");
+  else if (WTERMSIG(status) == SIGABRT)
+    dprintf(2, "Abort (core dumped)\n");
+  else if (WTERMSIG(status) == SIGFPE)
+    dprintf(2, "Floating exception (core dumped)\n");
+  else if (WTERMSIG(status) == SIGKILL)
+    dprintf(2, "Killed\n");
+  else if (WTERMSIG(status) == SIGALRM)
+    dprintf(2, "Alarm clock");
+  else if (WTERMSIG(status) == SIGPIPE)
+    dprintf(2, "Broken pipe\n");
+  else if (WTERMSIG(status) == SIGSTOP)
+    dprintf(2, "Suspended (signal)\n");
+  else if (WTERMSIG(status) == SIGTERM)
+    dprintf(2, "Terminated\n");
 }
